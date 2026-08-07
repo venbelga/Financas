e58eedf2e229d974e6dddfa787822f5ac3f8b8ba
venbelga <venanciobelga1@outlook.com>
2026-08-07 07:36:30 -0300

    Cria arquivo de rotas

--- App.tsx
+++ App.tsx
@@ -1,45 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import {StatusBar} from 'react-native';
+import Routes from './src/routes';
+import {NavigationContainer} from '@react-navigation/native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+export default function App(){
+  return(
+    <NavigationContainer>
+      <StatusBar backgroundColor="#f0f4ff" barStyle="dark-content" />
+      <Routes />
+    </NavigationContainer>
+  )
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, ActivityIndicator} from 'react-native';
+import AuthRoutes from './auth.routes';
+
+export default function Routes(){
+    const loading = false;
+    const signed = false;
+
+    return(
+        signed ? <View></View> : <AuthRoutes />
+    );
+}
